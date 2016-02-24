c395ed0548a574218a0a0b31596037146a5ac977
Kundavai1 <papadnandu@gmail.com>
2016-02-24 17:44:31 +0530

    Update route.js

--- MyProjects/ShoppingCart/src/js/Typed/route.js
+++ MyProjects/ShoppingCart/src/js/Typed/route.js
@@ -1,31 +1,28 @@
 /// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
 /// <reference path="../../scripts/typings/angularjs/angular-route.d.ts" />
-var shoppingCart;
-(function (shoppingCart) {
-    var Routes = (function () {
-        function Routes() {
-        }
-        Routes.configureRoutes = function ($routeProvider) {
+module shoppingCart {
+    export class Routes {
+        static $inject = ["$routeProvider"];
+        static configureRoutes($routeProvider: ng.route.IRouteProvider) {
             $routeProvider.
                 when('/store', {
-                templateUrl: 'partials/store.htm',
-                controller: shoppingCart.controllers.StoreController
-            }).
+                    templateUrl: 'Views/store.htm',
+                    controller: shoppingCart.controllers.StoreController
+                }).
                 when('/products/:productSku', {
-                templateUrl: 'partials/product.htm',
-                controller: shoppingCart.controllers.StoreController
-            }).
+                    templateUrl: 'Views/product.htm',
+                    controller: shoppingCart.controllers.StoreController
+
+                }).
                 when('/cart', {
-                templateUrl: 'partials/shoppingCart.htm',
-                controller: shoppingCart.controllers.StoreController
-            }).
+                    templateUrl: 'Views/shoppingCart.htm',
+                    controller: shoppingCart.controllers.StoreController
+
+                }).
                 otherwise({
-                redirectTo: '/store'
-            });
-        };
-        Routes.$inject = ["$routeProvider"];
-        return Routes;
-    })();
-    shoppingCart.Routes = Routes;
-})(shoppingCart || (shoppingCart = {}));
-//# sourceMappingURL=route.js.map
+                    redirectTo: '/store'
+                });
+        }
+
+    }
+} 
