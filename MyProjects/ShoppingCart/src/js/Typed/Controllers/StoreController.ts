d107d0f16c6d64bbd3a007f6ab0487e8e53b6588
Kundavai1 <papadnandu@gmail.com>
2016-02-24 17:40:11 +0530

    Update StoreController.ts

--- MyProjects/ShoppingCart/src/js/Typed/Controllers/StoreController.ts
+++ MyProjects/ShoppingCart/src/js/Typed/Controllers/StoreController.ts
@@ -1,13 +1,44 @@
-﻿/// <reference path="../../../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../../../scripts/typings/angularjs/angular.d.ts" />
 /// <reference path="../../../scripts/typings/angularjs/angular-route.d.ts" />
+/// <reference path="../services/productservice.ts" />
+/// <reference path="shoppingcartcontroller.ts" />
+
 
 module shoppingCart.controllers {
     export class StoreController {
-        static $inject = ["$scope,$routeParams"];
-        constructor($scope: ng.IScope, $routeParams: ng.route.IRouteParamsService) {
+
+
+
+        static $inject = ["$scope", "$routeParams", "ShoppingFactory"];
+
+        constructor(private $scope: any, private $routeParams: ng.route.IRouteParamsService, private shoppingFactory: any) {
+
+            $scope.store = shoppingFactory.store;
+            $scope.cart = shoppingFactory.cart;
+
+            var _self = this;
+            this.shoppingFactory.store.getProducts().then((responce) => {
+                $scope.store.products = responce.data[1];
+
+            }, (error) => {
+                console.log(error.message);
+            });
+
+
+            if (_self.$routeParams["productSku"] != null) {
+                this.shoppingFactory.store.getProduct(_self.$routeParams["productSku"]).then((responce) => {
+                    $scope.product = responce.data;
+
+                }, (error) => {
+                    console.log(error.message);
+                });
+
+            }
+
         }
+
     }
 
-    angular.module("shoppingCart").controller("shoppingCart.controllers.storeController", StoreController);
+    angular.module("shoppingCart").service("shoppingCart.controllers.storeController", StoreController);
 
 } 
